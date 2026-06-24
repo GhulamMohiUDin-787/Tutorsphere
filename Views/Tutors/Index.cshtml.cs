c1e477c8f48bcdf311a491a621ae7736d5a6d861
GhulamMohiUDin-787 <zainalikhan7822@gmail.com>
2026-06-24 13:13:05 +0500

    Add files via upload

---
+++ Views/Tutors/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace p1.Views.Tutors
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
